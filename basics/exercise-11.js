5764f63d784116aaebbcb1ab0468352c08a47fb0
Matt Pahuta <mhpahuta@gmail.com>
2021-03-24 10:16:02 -0700

    Exercises 11 and 12

---
+++ basics/exercise-11.js
@@ -0,0 +1,3 @@
+// What value does the following expression evaluate to? 
+parseInt('3.1415');
+// 3 - parseInt takes the string, pulls the first numeric value, stops at the first non-numeric ('.') and ignores the rest;
---
+++ basics/exercise-12.js
@@ -0,0 +1,5 @@
+// What does the following expression evaluate to?
+'12' < '9'
+
+// This evaluates to true because the expression in comparing strings and JavaScript compares character-by-character, left to right
+// JS is evaluating 1 < 9 
