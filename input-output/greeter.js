1b7a674b8009f68b42ecca55e228d954d83091f8
Matt Pahuta <mhpahuta@gmail.com>
2021-03-25 10:52:29 -0700

    Input-output exercises

---
+++ input-output/greeter.js
@@ -0,0 +1,11 @@
+// Exercise 1 - Input/Output
+// Write a dynamic greeter program
+// let readlineSync = require('readline-sync');
+// let userName = readlineSync.question('What is your name? ');
+// console.log(`Hello, ${userName}!`);
+
+// Exercise 2
+let readlineSync = require('readline-sync');
+let firstName = readlineSync.question('What is your first name? ');
+let lastName = readlineSync.question('What is your last name? ');
+console.log(`Hello, ${firstName} ${lastName}!`);
